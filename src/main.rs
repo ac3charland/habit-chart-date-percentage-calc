4a8bafbfd5b08e42ef59477f55787920c819a71e
Alex Charland <ac3charland@gmail.com>
2023-11-28 08:06:07 -0600

    Finish program

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use chrono::{Local, NaiveDate};
+
 fn main() {
     // Get the current date
-    let today = Local::today();
+    let today = Local::now().date_naive();
 
     // Create a date for October 15th, 2024
-    let target_date = Local::ymd(2024, 10, 15);
+    let target_date = NaiveDate::from_ymd_opt(2024, 10, 15).expect("Invalid date");
 
     // Calculate the duration between today and the target date
     let duration = target_date.signed_duration_since(today);
@@ -11,8 +13,9 @@ fn main() {
     // Extract the number of days from the duration
     let days = duration.num_days();
 
-    let percentage = (365 - days) / 365 * 100;
+    // Calculate percentage
+    let percentage = (365 - days) as f32 * 100.0 / 365.0;
 
     // Print the result
-    println!("Percentage complete until October 15th, 2024:\n\n {}\n\n==============================", percentage);
+    println!("\nPercentage complete until October 15th, 2024:\n\n {}%\n\n=============================================\n", percentage.round());
 }
